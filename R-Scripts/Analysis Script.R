0517b26934f90d8b970d44cb56f22511c9a6c328
Cody West <cww2697@gmail.com>
2020-04-25 18:19:55 -0500

    Update Analysis Script.R

--- R-Scripts/Analysis Script.R
+++ R-Scripts/Analysis Script.R
@@ -38,6 +38,8 @@ rm(Stream)
 rm(Stream_flat)
 rm(jsonfile)
 
+
+
 #Build Network Graph
 edges <-stream_tbl$in_reply_to_status_id # Create edge list from Reply to id string
 edges <-replace_na(edges, "NA") # Replace NA items with "NA" string
@@ -58,7 +60,7 @@ sm.min_index <- which.min(sm_centrality)
 sm.max_index <- which.max(sm_centrality)
 
 
-
+ts_plot(stream_tbl, by = "days", trim = 0L, tz = "UTC", ...)
 
 # Delete single node communities, NA Node, and recompute communities
 sm_graph <- delete.vertices(social_media_graph, "NA")
@@ -74,6 +76,68 @@ sm_new_closeness
 sm_pr_cent <- proper_centralities(sm_graph)
 sm_hub.score <- hub_score(sm_graph)
 sm_authority.score <- authority_score(sm_graph)
-sm_sub_cent <- subgraph_centrality(sm_graph)
+
+# pull twitter data using rtweet
+sample.set <- sample(stream_tbl$id_str, size = 200, replace = FALSE, prob = NULL)
+sample.statuses <- lookup_statuses(sample.set)
+sample.retweet.avg <- mean(sample.statuses$retweet_count)
+sample.favorite.avg <- mean(sample.statuses$favorite_count)
+sample.follower.avg <- mean(sample.statuses$followers_count)# Read in JSON Files
+Stream <- stream_in(file(jsonfile))
+Stream_flat <- jsonlite::flatten(Stream) #Flatten the JSON Filw into a table
+stream_tbl <- as_tibble(Stream_flat) #create a dataframe from the flattened JSON Table
+stream_tbl = stream_tbl[!grepl("RT", stream_tbl$text),]  #Removes all retweets from the dataframe
+stream_tbl = stream_tbl[!(!stream_tbl$lang=="en"),]  #remove all tweets that are not in english
+
+# Remove unused tables
+rm(Stream)
+rm(Stream_flat)
+rm(jsonfile)
+
+
+
+#Build Network Graph
+edges <-stream_tbl$in_reply_to_status_id # Create edge list from Reply to id string
+edges <-replace_na(edges, "NA") # Replace NA items with "NA" string
+social_media_graph <- graph(edges, n=max(edges)+1, directed=FALSE) # Create the graph
+
+# Calculate communities from the data
+sm_community <- cluster_walktrap(social_media_graph)
+SM_Mod <- modularity(sm_community)
+SM_Mem <- membership(sm_community)
+sm_centrality <- centr_degree(social_media_graph, mode = "all")
+sm_closeness <- closeness(social_media_graph, mode="all")
+sm_centrality$res
+sm_closeness
+sm.max <- max(sm_centrality)
+sm.min <- min(sm_centrality)
+sm.range <- range(sm_centrality)
+sm.min_index <- which.min(sm_centrality)
+sm.max_index <- which.max(sm_centrality)
+
+
+ts_plot(stream_tbl, by = "days", trim = 0L, tz = "UTC", ...)
+
+# Delete single node communities, NA Node, and recompute communities
+sm_graph <- delete.vertices(social_media_graph, "NA")
+sm_graph <- delete.vertices(simplify(sm_graph), degree(sm_graph)==1)
+sm_graph <- delete.vertices(sm_graph, "NA")
+sm_new_community <- cluster_walktrap(sm_graph)
+sm_new_mod <- modularity(sm_new_community)
+sm_new_mem <- membership(sm_new_community)
+sm_new_centrality <- centr_degree(sm_graph, mode = "all")
+sm_new_closeness <- closeness(sm_graph, mode="all")
+sm_new_centrality$res
+sm_new_closeness
+sm_pr_cent <- proper_centralities(sm_graph)
+sm_hub.score <- hub_score(sm_graph)
+sm_authority.score <- authority_score(sm_graph)
+
+# pull twitter data using rtweet
+sample.set <- sample(stream_tbl$id_str, size = 200, replace = FALSE, prob = NULL)
+sample.statuses <- lookup_statuses(sample.set)
+sample.retweet.avg <- mean(sample.statuses$retweet_count)
+sample.favorite.avg <- mean(sample.statuses$favorite_count)
+sample.follower.avg <- mean(sample.statuses$followers_count)
 
 
